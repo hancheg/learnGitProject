fe11bdb09690f72380a784179261c0e45e717d77
林汉城 <axia@linhanchengdeMacBook-Pro.local>
2022-10-23 12:45:22 +0800

    add slice & string slice & hash

--- main.go
+++ main.go
@@ -5,5 +5,12 @@ import "fmt"
 func main() {
 	fmt.Println("vim-go")
 	fmt.Println("amend")
+	s := []int{1, 2, 3, 4}
+	fmt.Println(s)
+	str := []string{"123", "456"}
+	fmt.Println(str)
+	hash := make(map[int]int, 0)
+	hash[1] = 1
+	fmt.Println(hash)
 
 }
